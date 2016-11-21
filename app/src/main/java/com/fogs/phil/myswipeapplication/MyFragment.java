0bd6be188c8ff87ad933535e7e84bb44e85e547b
Phil <pflavin@gmail.com>
2016-11-21 15:36:01 +0000

    Initial commit with 2 swipe fragments working

---
+++ app/src/main/java/com/fogs/phil/myswipeapplication/MyFragment.java
@@ -0,0 +1,46 @@
+package com.fogs.phil.myswipeapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Phil on 21/11/2016.
+ */
+
+public class MyFragment extends Fragment {
+
+    public static MyFragment newInstance() {
+        MyFragment fragment = new MyFragment();
+        return fragment;
+    }
+
+    public MyFragment(){
+    }
+
+    Button ClickMe;
+    TextView tv;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.my_fragment, container, false);
+        ClickMe = (Button) rootView.findViewById(R.id.button);
+        tv = (TextView) rootView.findViewById(R.id.textView2);
+
+        ClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tv.getText().toString().contains("Hello")){
+                    tv.setText("Hi");
+                }else tv.setText("Hello");
+            }
+        });
+
+    return rootView;
+    }
+}
